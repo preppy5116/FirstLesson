94e54142a6c298a2e89a10638333c85a19d4a6d9
preppy5116 <61729850+preppy5116@users.noreply.github.com>
2023-12-21 19:01:13 +0300

    Add files via upload
    Забыто что класс абстрактный

---
+++ FirstDZ/FirstDZ/src/Main.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Child first = new Child();
+        Child second = new Child("Ivan");
+       }
+}
+
+abstract class  Parent {
+    private String name;
+    static {
+        out.println("Parent:static 1");
+    }
+
+    {
+        out.println("Parent:instance 1");
+    }
+
+    static {
+        out.println("Parent:static 2");
+    }
+    Parent () {
+        out.println("Parent:constructor");
+    }
+
+    {
+        out.println("Parent:instance 2");
+    }
+    Parent (String name) {
+        out.println("Parent:name-constructor");
+
+    }
+}
+class Child extends Parent {
+    static {
+        out.println("Child:static 1");
+    }
+
+    {
+        out.println("Child:instance 1");
+    }
+
+    static {
+        out.println("Child:static 2");
+    }
+    Child () {
+        out.println("Child:constructor");
+    }
+
+    Child (String name) {
+        out.println("Child:name-constructor");
+
+    }
+    {
+        out.println("Child:instance 2");
+    }
+}Ы
